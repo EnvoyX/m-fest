6e5ea0e48c334a36860f12d8ff81f1ff6252421b
EnvoyX <muhamadhanifhafizhan@gmail.com>
2026-01-15 10:09:21 +0700

    style: fix sidebar style on mobile

--- src/components/admin/sidebar/app-sidebar.tsx
+++ src/components/admin/sidebar/app-sidebar.tsx
@@ -2,56 +2,62 @@
 import { NavMain } from "@/components/admin/sidebar/nav-main";
 import { NavUser } from "@/components/admin/sidebar/nav-user";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import { dataNavSidebarAdmin as dataNavSidebar } from "@/constants/constants";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar
-      collapsible="offcanvas"
-      {...props}
-      className="bg-transparent backdrop-blur-lg border-r-1"
-    >
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-8 hover:scale-105 transition-all"
-            >
-              <Link
-                href="/"
-                aria-label="home"
-                className="flex items-center gap-4 space-x-2"
-                prefetch
-              >
-                <Image
-                  src="/logo.svg"
-                  alt="Mechanical Festival 2025"
-                  width={80}
-                  height={80}
-                />
-                <Image src="/hmm.png" alt="HMM ITB" width={45} height={45} />
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent className="bg-transparent backdrop-blur-3xl ">
-        <NavMain items={dataNavSidebar.navMain} />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser />
-      </SidebarFooter>
-    </Sidebar>
-  );
+    return (
+        <Sidebar
+            collapsible="offcanvas"
+            {...props}
+            className="bg-transparent backdrop-blur-lg border-r-1"
+            variant="floating"
+        >
+            <SidebarHeader className="bg-transparent backdrop-blur-3xl">
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-8 hover:scale-105 hover:bg-white/15  transition-all"
+                        >
+                            <Link
+                                href="/"
+                                aria-label="home"
+                                className="flex items-center gap-4 space-x-2"
+                                prefetch
+                            >
+                                <Image
+                                    src="/logo.svg"
+                                    alt="Mechanical Festival 2025"
+                                    width={80}
+                                    height={80}
+                                />
+                                <Image
+                                    src="/hmm.png"
+                                    alt="HMM ITB"
+                                    width={45}
+                                    height={45}
+                                />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent className="bg-transparent backdrop-blur-3xl ">
+                <NavMain items={dataNavSidebar.navMain} />
+            </SidebarContent>
+            <SidebarFooter className="bg-transparent backdrop-blur-3xl">
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
