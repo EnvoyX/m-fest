4234a9340737ab51709a6e315d6fd66c6f8f61fe
Handthere <152689692+Handthere@users.noreply.github.com>
2026-01-18 10:25:09 +0700

    style: change photos for event section

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -2,7 +2,7 @@ export const events = [
     {
         title: "M-Care",
         desc: "We want to show that we care, Save Blood Save Lives! Register yourself, donate your blood, and get a free medical check up!",
-        img: "/events/card/mcare.png",
+        img: "/events/card/mcare.JPG",
         logo: "/events/mcare-icon.svg",
         href: "/events/m-care",
     },
@@ -23,21 +23,21 @@ export const events = [
     {
         title: "M-Talks",
         desc: "Delve deep into the conversation with professionals and experts to better understand the issues we're facing.",
-        img: "/events/card/mtalks.png",
+        img: "/events/card/mtalks.JPG",
         logo: "/events/mtalks-icon.svg",
         href: "/events/m-talks",
     },
     {
         title: "M-Expo",
         desc: "Meet some exciting mechanical related companies, projects, machinery, and so many more in this mechanical exhibition.",
-        img: "/events/card/mexpo.png",
+        img: "/events/card/mexpo.JPG",
         logo: "/events/mexpo-icon.svg",
         href: "/events/m-expo",
     },
     {
         title: "Ceremony",
         desc: "End this journey with exhilarating celebration, together with us and our performers!",
-        img: "/events/card/ceremony.png",
+        img: "/events/card/ceremony.JPG",
         logo: "/events/ceremony-icon.svg",
         href: "/events/ceremony",
     },
