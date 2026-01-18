037bacae1848a4234bd8d0184521b87687d83c8a
EnvoyX <muhamadhanifhafizhan@gmail.com>
2026-01-18 14:23:28 +0700

    open register

--- src/app/(general)/(competitions)/competitions/CompetitionList.tsx
+++ src/app/(general)/(competitions)/competitions/CompetitionList.tsx
@@ -63,29 +63,15 @@ export default function CompetitionsList() {
                             variant="secondary"
                             size="lg"
                             className="rounded-lg text-base font-bold px-6 py-6"
-                            disabled={
-                                competition.startRegDate1 > currentDate ||
-                                competition.endRegDate3 < currentDate
-                            }
                         >
-                            {competition.startRegDate1 > currentDate ? (
-                                <span className="cursor-not-allowed opacity-50 flex items-center gap-2">
-                                    Coming Soon <Clock className="size-5" />
-                                </span>
-                            ) : competition.endRegDate3 < currentDate ? (
-                                <span className="cursor-not-allowed opacity-50">
-                                    Registration Closed
-                                </span>
-                            ) : (
-                                <Link
-                                    href={`/dashboard/team`}
-                                    prefetch
-                                    className="flex items-center gap-2"
-                                >
-                                    <span>Register Now</span>
-                                    <ChevronRight className="size-5" />
-                                </Link>
-                            )}
+                            <Link
+                                href={`/dashboard/team`}
+                                prefetch
+                                className="flex items-center gap-2"
+                            >
+                                <span>Register Now</span>
+                                <ChevronRight className="size-5" />
+                            </Link>
                         </Button>
                     </div>
                 </BlurFade>
