c38fad8e3cd25877282a92f6182dc4523174ab7a
D4reRe <d4remisc@gmail.com>
2026-01-27 21:01:51 +0700

    added mcare and mrun page

--- src/app/(general)/events/m-care/page.tsx
+++ src/app/(general)/events/m-care/page.tsx
@@ -1,25 +1,94 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function MCare() {
   return (
-    <div>
+    <div className="w-full">
       <div>
-        <div className="bg-[url('/mcareslice.png')] bg-contain bg-no-repeat bg-center aspect-video" />
-        <div className="flex justify-end absolute">
-          <h1 className="font-semibold text 6-xl">M-Care</h1>
-          <p className="">
-            {" "}
-            Willing to lend a hand or two? Make sure to drop by
-          </p>
-        </div>
+        <div className="bg-[url('/mcareslice1.png')] bg-contain bg-no-repeat bg-center aspect-video" />
       </div>
 
       <div className="py-16 w-full">
-        <h1 className="text-7xl font-semibold text-center pb-16">
+        <h1 className="text-5xl lg:text-7xl font-semibold text-center pb-16">
           Klinik Mesin
         </h1>
-        <div className="bg-[url('/mcareimage1.png')] w-1/2 h-1/2 aspect-video bg-no-repeat mx-auto" />
-        <h1 className="text-center text-2xl">
+        <Image
+          src="/mcareimage1.png"
+          alt="mcareimage1"
+          height={1388}
+          width={925}
+          className="mx-auto"
+        />
+        <h1 className="text-lg lg:text-xl lg:mx-80">
           One of our event consisting free eye check-up and blood donation
         </h1>
+      </div>
+
+      <div className="mx-24 lg:mx-78">
+        <h1 className="font-semibold text-4xl lg:text-5xl mb-12">
+          {" "}
+          Donor Requirements
+        </h1>
+        <ul className="list-disc list-inside space-y-5 text-2xl pl-5 mb-12">
+          <li>18 years or older</li>
+          <li>Ideal body weight, minimum body weight of 50kg</li>
+          <li>
+            Memiliki kondisi kesehatan yang baik / tidak sedang dalam pengobatan
+          </li>
+          <li>Tidak melakukan prosedur pembedahan</li>
+          <li>Tidur dengan cukup sebelum prosedur minimal 5 jam</li>
+          <li>Tidak mengkonsumsi alkohol/obat paracetamol/antibiotik</li>
+          <li>
+            Tidak mengkonsumsi makanan dengan kandungan tinggi lemak jenuh
+          </li>
+          <li>
+            Tidak ada riwayat penyakit menular seperti heptatitis, TB, HIV/AIDS
+          </li>
+        </ul>
+        <p className="text-lg lg:text-xl">
+          No requirement for those attending only the eye check-up
+        </p>
+        <div className="z-10 relative w-1/2 h-1/2 bg-white">
+          <Button asChild variant="secondary" size="lg" className="mx-auto">
+            Register Here
+          </Button>
+        </div>
+      </div>
+
+      <div className="pt-16">
+        <h1 className="text-6xl lg:text-7xl font-semibold text-center">
+          Hari Main Bersama
+        </h1>
+        <h1 className="italic text-3xl lg:text-4xl text-center">
+          Pertualangan Rimba, Warna dalam Cerita.
+        </h1>
+        <div className="mt-16 mb-6">
+          <Image
+            src="/mcareimg2.png"
+            alt="image2"
+            width={925}
+            height={1388}
+            className="mx-auto"
+          />
+        </div>
+        <div className="mx-8 lg:mx-80">
+          <h1 className="text-lg lg:text-xl font-thin">
+            Melihat Lebih Baik, Memberi Lebih Banyak. Tema ini mencerminkan
+            makna dari dua kegiatan Klinik Mesin yaitu Melihat Lebih Baik
+            sebagai ajakan untuk menjaga kesehatan mata dan Memberi Lebih Banyak
+            sebagai wujud nyata kepedulian terhadap sesama melalui donor darah.
+          </h1>
+        </div>
+      </div>
+
+      <div className="py-16">
+        <Image
+          src="/mcaretimeline.png"
+          alt="M-Care Timeline"
+          height={900}
+          width={770}
+          className="mx-auto"
+        />
       </div>
     </div>
   );
--- src/app/(general)/events/m-run/page.tsx
+++ src/app/(general)/events/m-run/page.tsx
@@ -1,12 +1,86 @@
+import Image from "next/image";
+
 export default function ComingSoon() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-zinc-950 text-white">
-      <div className="text-center space-y-4">
-        <h1 className="text-6xl font-black tracking-tighter">M-FEST</h1>
-        <p className="text-zinc-400 text-lg uppercase tracking-[0.2em]">
-          Launching Soon
-        </p>
-        <div className="h-1 w-24 bg-blue-600 mx-auto rounded-full" />
+    <div className="w-full">
+      <div>
+        <div className="bg-[url('/mrunslice1.png')] bg-contain bg-no-repeat bg-center aspect-video" />
+      </div>
+
+      <div className="py-16 w-full">
+        <h1 className="text-7xl font-bold text-center"> M-Run</h1>
+        <Image
+          src="/mrunimage1.png"
+          alt="mrunimage1"
+          height={1378}
+          width={926}
+          className="mx-auto pt-16 pb-5"
+        />
+        <div className="mx-80">
+          <h1 className="text-3xl">
+            Ever felt your heart pounding like a powerful engine, your breath
+            flowing like a perfectly tuned air intake system, and your legs
+            driving you forward like a high-performance transmission?
+          </h1>
+          <h1 className="font-semibold text-3xl pt-8">
+            At M-RUN 2026, you’re not just running—you’re operating as the most
+            advanced biological machine ever built: YOURSELF.
+          </h1>
+        </div>
+      </div>
+      <div className="mx-80 ">
+        <h1 className="text-xl">The race will be on going on</h1>
+        <h1 className="text-5xl">From now, that is roughly</h1>
+      </div>
+
+      <div className="py-16">
+        <h1 className="mx-64 text-9xl font-bold">
+          With the Grand Prize Totalling...
+        </h1>
+        <Image
+          src="/mrunimage2.png"
+          alt="mrunimage2"
+          height={1000}
+          width={4000}
+          className="py-16"
+        />
+      </div>
+
+      <div className="mx-64">
+        <h1 className="text-7xl pb-5 font-semibold">Other Information</h1>
+        <p className="text-3xl ">Distance 5km</p>
+        <p className="text-3xl">Route : Institut Teknologi Bandung</p>
+        <p className="text-3xl">Facility :</p>
+        <ul className="space-y-3 text-xl list-disc list-inside ml-5">
+          <li>M-Run 2026 Jersey</li>
+          <li>Finisher Medal</li>
+          <li>BIB</li>
+          <li>Refreshments</li>
+          <li>Doorprize Coupon</li>
+        </ul>
+      </div>
+
+      <div className="py-16">
+        <h1 className="text-6xl mx-24 py-3">Ready to operate at</h1>
+        <h1 className="mx-24 text-6xl">full throttle?</h1>
+        <Image
+          src="/mrunimage3.png"
+          alt="mrunimage3"
+          width={1000}
+          height={467}
+          className="mx-auto pt-8"
+        />
+      </div>
+
+      <div>
+        <Image
+          src="/mruntimeline.png"
+          alt="mruntimeline"
+          height={600}
+          width={1200}
+          className="mx-auto"
+        />
+        <p className="italic text-xl mx-64 pt-6">gotta go fast</p>
       </div>
     </div>
   );
