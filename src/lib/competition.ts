0f49d2e4c5c968fc0a984435d5fa6f4ad65e50d2
Handthere <152689692+Handthere@users.noreply.github.com>
2026-02-07 17:11:20 +0700

    closereg for stem

--- src/lib/competition.ts
+++ src/lib/competition.ts
@@ -160,12 +160,13 @@ export const competitions: {
             reg1: "18 January - 24 January 2026", // Early Bird
             reg2: "25 January - 15 February 2026", // Regular
             reg3: "16 Ferbuary - 22 February 2026", // Extended Regular
-            startRegDate1,
-            startRegDate2,
-            startRegDate3,
-            endRegDate1,
-            endRegDate2,
-            endRegDate3,
+           // Closed Reg for PDC
+            startRegDate1: new Date("2026-01-01T14:00:00"),
+            startRegDate2: new Date("2026-01-01T00:00:00"),
+            startRegDate3: new Date("2026-01-01T00:00:00"),
+            endRegDate1: new Date("2026-01-02T23:59:59"),
+            endRegDate2: new Date("2026-01-02T23:59:59"),
+            endRegDate3: new Date("2026-01-02T23:59:59"),
             fee1: 120001, // Early Bird
             fee2: 180001, // Regular
             guideBook:
