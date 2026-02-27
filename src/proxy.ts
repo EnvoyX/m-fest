582d180f00230b5438934e011994515f26d78f29
EnvoyX <muhamadhanifhafizhan@gmail.com>
2026-02-27 14:14:32 +0700

    fix: added trailing slash on proxy.ts's redirect

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,39 +4,39 @@ import { adminRoles } from "@/constants/constants";
 import { auth } from "@/server/auth/auth";
 
 export default async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
+    const session = await auth.api.getSession({
+        headers: request.headers,
+    });
 
-  const protectedPaths = ["/dashboard", "/admin"];
-  const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
+    const protectedPaths = ["/dashboard", "/admin"];
+    const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
 
-  if (isProtected && !session) {
-    const loginUrl = new URL("/login", request.nextUrl.origin);
-    loginUrl.searchParams.set("callbackUrl", encodeURI(pathname));
-    const response = NextResponse.redirect(loginUrl);
-    response.headers.set("x-middleware-next", "1");
-    // response.headers.set("Cache-Control", "no-store, max-age=0");
-    return response;
-  }
+    if (isProtected && !session) {
+        const loginUrl = new URL("/login", request.nextUrl.origin);
+        loginUrl.searchParams.set("callbackUrl", encodeURI(`${pathname}/`));
+        const response = NextResponse.redirect(loginUrl);
+        response.headers.set("x-middleware-next", "1");
+        // response.headers.set("Cache-Control", "no-store, max-age=0");
+        return response;
+    }
 
-  if (
-    pathname === "/admin" &&
-    session &&
-    !adminRoles.includes(session.user.role as string)
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", request.nextUrl.origin));
-  }
+    if (
+        pathname === "/admin" &&
+        session &&
+        !adminRoles.includes(session.user.role as string)
+    ) {
+        return NextResponse.redirect(new URL("/dashboard", request.nextUrl.origin));
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/admin/:path*",
-    "/((?!api|_next/static|_next/image|.*\\.png$).*)",
-  ],
+    matcher: [
+        "/dashboard/:path*",
+        "/admin/:path*",
+        "/((?!api|_next/static|_next/image|.*\\.png$).*)",
+    ],
 };
