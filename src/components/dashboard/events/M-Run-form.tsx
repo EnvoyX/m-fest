e644a7e8ca2902670bdefeeffd79386e1202d6a5
EnvoyX <muhamadhanifhafizhan@gmail.com>
2026-03-13 16:06:04 +0700

    fix: change from eng to idn

--- src/components/dashboard/events/M-Run-form.tsx
+++ src/components/dashboard/events/M-Run-form.tsx
@@ -673,11 +673,11 @@ export default function MRunForm() {
                                 Batch {batchInfo?.batch} {form.watch("category") && ` | Harga : Rp. ${form.watch("category") === "UMUM" ? batchInfo?.pricePublic : batchInfo?.priceStudent}`}
                             </h4>
                     <h3 className="text-base font-bold text-start mb-3">
-                    Registration fees should be paid through the following:
+                    Pembayaran melalui transfer ke:
                     <p className="flex flex-col items-start justify-center">
-                        <span>Account Bank Name: SEABANK </span>
-                        <span>Account Number: 901914836624 </span>
-                        <span>Holder: Reva Elita Nurhaliza</span>
+                        <span>Bank Terdaftar: SEABANK </span>
+                        <span>Nomor Rekening: 901914836624 </span>
+                        <span>Nama Pemilik Rekening: Reva Elita Nurhaliza</span>
                     </p>
                     </h3>
                             <FormField
