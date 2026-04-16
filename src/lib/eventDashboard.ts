8c720f7043c95a322d666cc51038c0771debe0f5
Handthere <152689692+Handthere@users.noreply.github.com>
2026-04-16 11:33:24 +0700

    closereg

--- src/lib/eventDashboard.ts
+++ src/lib/eventDashboard.ts
@@ -73,13 +73,13 @@ export const eventsList: Event[] = [
         eventDate: "Minggu, 3 Mei 2026",
         location: "Institut Teknologi Bandung Ganesha",
         startRegDate1: new Date("2026-04-05T13:00:00"),
-        endRegDate1: new Date("2026-04-16T12:00:00"),
+        endRegDate1: new Date("2026-04-16T11:30:00"),
         // Batch 1 : 13 Maret - 4 April
         // Batch 2 : 5 April - 11 April 
         Batch1StartRegDate: new Date("2026-03-13T15:00:00"),
         Batch1EndRegDate: new Date("2026-04-04T23:59:59"),
         Batch2StartRegDate: new Date("2026-04-05T00:00:00"),
-        Batch2EndRegDate: new Date("2026-04-16T12:00:00"),
+        Batch2EndRegDate: new Date("2026-04-16T11:30:00"),
         // Batch 1
         price1: 120001, // Umum
         price2: 110001, // Mahasiswa
